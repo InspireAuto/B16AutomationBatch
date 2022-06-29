62285ce99c77046c0c393ba5743d6b0de44804e4
InspireTesting2022 <akashtiwari1202@gmail.com>
2022-06-29 21:33:40 +0530

    I have added new test case

---
+++ src/test/java/test/DemoTestCase.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class DemoTestCase {
+	
+	
+	@Test
+	public void loginTestCase1() {
+		System.out.println("First test case");
+	}
+
+	@Test
+	public void loginTestCase2() {
+		System.out.println("Second test case");
+	}
+	
+	@Test
+	public void loginTestCase3() {
+		System.out.println("Third test case");
+	}
+
+	@Test
+	public void loginTestCase4() {
+		System.out.println("Fourth test case");
+	}
+
+}
